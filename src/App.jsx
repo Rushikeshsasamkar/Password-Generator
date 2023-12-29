ca5e9b298825eeb0dfe61037b50e560cd2377b19
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-12-29 14:47:47 +0530

    implementing useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@ import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [length,setLength] = useState(8);
+  const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed,setCharAllowed] = useState(false);
-  const [Password,setPasswoed] = useState("")
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [Password, setPassword] = useState("")
 
 
-  const passwordGenerator = useCallback(fn,[])
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*{}()~"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed, setPassword])
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-purple-700'>
+        <h1>test</h1>
+      </div>
     </>
   )
 }
